73aa3713aaf928dd4c7831f542548a9e69f81354
xwx <1162027477@qq.com>
2021-12-29 10:45:49 +0800

    更新dao层

--- re_dao/src/main/java/com/recruitment/dao/mapper/EvaluationMapper.java
+++ re_dao/src/main/java/com/recruitment/dao/mapper/EvaluationMapper.java
@@ -1,13 +1,16 @@
 package com.recruitment.dao.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.recruitment.dao.domain.Evaluation;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.List;
 
 /**
  * @Entity com.recruitment.dao.domain.Evaluation
  */
 public interface EvaluationMapper extends BaseMapper<Evaluation> {
-
+    List<Evaluation> selectList();
 }
 
 
