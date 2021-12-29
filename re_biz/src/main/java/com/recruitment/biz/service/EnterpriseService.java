3b6e050dec1771d9b0ee5fd14461cca96cd60e41
xwx <1162027477@qq.com>
2021-12-29 14:09:49 +0800

    修复

---
+++ re_biz/src/main/java/com/recruitment/biz/service/EnterpriseService.java
@@ -0,0 +1,8 @@
+package com.recruitment.biz.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.recruitment.dao.domain.Enterprise;
+
+
+public interface EnterpriseService extends IService<Enterprise> {
+}
---
+++ re_biz/src/main/java/com/recruitment/biz/service/EvaluationService.java
@@ -0,0 +1,7 @@
+package com.recruitment.biz.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.recruitment.dao.domain.Evaluation;
+
+public interface EvaluationService extends IService<Evaluation> {
+}
