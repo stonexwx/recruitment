a2e054ac4596aaab1c1d69f88a775965bf5c610a
xwx <1162027477@qq.com>
2021-12-29 14:04:35 +0800

    修复

--- re_biz/src/main/java/com/recruitment/biz/service/impl/EnterpriseServiceImpl.java
+++ re_biz/src/main/java/com/recruitment/biz/service/impl/EnterpriseServiceImpl.java
@@ -1,8 +1,9 @@
 package com.recruitment.biz.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.recruitment.dao.domain.Enterprise;
 import com.recruitment.biz.service.EnterpriseService;
+import com.recruitment.dao.domain.Enterprise;
+
 import com.recruitment.dao.mapper.EnterpriseMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,7 @@ import java.util.List;
  *
  */
 @Service("en")
-public class EnterpriseServiceImpl extends ServiceImpl<EnterpriseMapper, Enterprise> implements EnterpriseService{
+public class EnterpriseServiceImpl extends ServiceImpl<EnterpriseMapper, Enterprise> implements EnterpriseService {
 
 }
 
