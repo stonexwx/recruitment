44bce5b4229509e170f1293ca8d58178c430bf42
xwx <1162027477@qq.com>
2021-12-30 20:41:49 +0800

    招聘信息查询全部controll完成

--- re_web/src/main/java/com/recruitment/controller/ReinfoConller.java
+++ re_web/src/main/java/com/recruitment/controller/ReinfoConller.java
@@ -2,7 +2,7 @@ package com.recruitment.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.recruitment.biz.service.impl.ReInfoServiceImpl;
-import com.recruitment.dao.domain.ReInfo;
+import com.recruitment.dao.dto.ReinfoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +17,9 @@ public class ReinfoConller {
     @RequestMapping("/reinfoAll")
     @ResponseBody
     public String reinfoSelect(){
-        List<ReInfo> list =reInfoService.selectALL();
+        List<ReinfoDTO> list =reInfoService.selectALL();
         String jsonUser = JSON.toJSONString(list);
-
+        System.out.println(jsonUser);
         return jsonUser;
     }
 }
