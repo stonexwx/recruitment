efc8ed3860ce8c3a4fad3c2d2985281b011926f1
xwx <1162027477@qq.com>
2021-12-31 10:43:21 +0800

    求职者信息更新

--- re_biz/src/main/java/com/recruitment/biz/service/JobService.java
+++ re_biz/src/main/java/com/recruitment/biz/service/JobService.java
@@ -2,6 +2,9 @@ package com.recruitment.biz.service;
 
 import com.recruitment.dao.domain.Job;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.recruitment.dao.domain.JobSeeker;
+
+import java.util.List;
 
 /**
  *
--- re_biz/src/main/java/com/recruitment/biz/service/impl/JobServiceImpl.java
+++ re_biz/src/main/java/com/recruitment/biz/service/impl/JobServiceImpl.java
@@ -3,8 +3,13 @@ package com.recruitment.biz.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.recruitment.dao.domain.Job;
 import com.recruitment.biz.service.JobService;
+import com.recruitment.dao.domain.JobSeeker;
 import com.recruitment.dao.mapper.JobMapper;
+import com.recruitment.dao.mapper.JobSeekerMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  *
