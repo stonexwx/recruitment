a182d9cd52f9c5b38877816e6b48b53f2814dbfd
xwx <1162027477@qq.com>
2021-12-31 10:42:58 +0800

    求职者信息更新

--- re_biz/src/main/java/com/recruitment/biz/service/JobSeekerService.java
+++ re_biz/src/main/java/com/recruitment/biz/service/JobSeekerService.java
@@ -2,10 +2,13 @@ package com.recruitment.biz.service;
 
 import com.recruitment.dao.domain.JobSeeker;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.recruitment.dao.dto.JobSeekerDTO;
+
+import java.util.List;
 
 /**
  *
  */
 public interface JobSeekerService extends IService<JobSeeker> {
-
+    List<JobSeekerDTO> seekerSelect(long uid);
 }
--- re_biz/src/main/java/com/recruitment/biz/service/impl/JobSeekerServiceImpl.java
+++ re_biz/src/main/java/com/recruitment/biz/service/impl/JobSeekerServiceImpl.java
@@ -3,8 +3,12 @@ package com.recruitment.biz.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.recruitment.dao.domain.JobSeeker;
 import com.recruitment.biz.service.JobSeekerService;
+import com.recruitment.dao.dto.JobSeekerDTO;
 import com.recruitment.dao.mapper.JobSeekerMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  *
@@ -12,7 +16,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class JobSeekerServiceImpl extends ServiceImpl<JobSeekerMapper, JobSeeker>
     implements JobSeekerService{
+    @Autowired
+    JobSeekerMapper jobSeekerMapper;
 
+    /**
+     * 根据id查询求职者信息
+     * @param uid
+     * @return
+     */
+    @Override
+    public List<JobSeekerDTO> seekerSelect(long uid) {
+        return jobSeekerMapper.seekerSelect(uid);
+    }
 }
 
 
